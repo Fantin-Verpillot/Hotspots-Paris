4841d116d11878368a905e38b6ad8fb06a656be1
MrLyfing <ilyfing@gmail.com>
2016-06-12 18:59:10 +0200

    search bar display

--- app/src/main/java/com/epita/parishotspot/MainActivity.java
+++ app/src/main/java/com/epita/parishotspot/MainActivity.java
@@ -1,10 +1,13 @@
 package com.epita.parishotspot;
 
 import android.app.ProgressDialog;
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
@@ -96,6 +99,17 @@ public class MainActivity extends AppCompatActivity implements ListView.OnItemCl
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.action_search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
+
         return true;
     }
 
@@ -120,7 +134,6 @@ public class MainActivity extends AppCompatActivity implements ListView.OnItemCl
             startActivity(intent);
             return true;
         }
-
 
 
         return super.onOptionsItemSelected(item);
