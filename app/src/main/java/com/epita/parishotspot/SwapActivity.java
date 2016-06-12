36c180a70b77128aaad15246ec8bde713f87f325
Fantin VERPILLOT <fantin.verpillot@epita.fr>
2016-06-12 21:53:08 +0200

    merge philippe

--- app/src/main/java/com/epita/parishotspot/SwapActivity.java
+++ app/src/main/java/com/epita/parishotspot/SwapActivity.java
@@ -1,10 +1,13 @@
 package com.epita.parishotspot;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class SwapActivity extends AppCompatActivity {
 
@@ -24,5 +27,30 @@ public class SwapActivity extends AppCompatActivity {
         ViewPager viewPager = (ViewPager) findViewById(R.id.container);
         viewPager.setAdapter(swapAdapter);
         viewPager.setCurrentItem((Integer) getIntent().getSerializableExtra("recordPos"));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_swap, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_share) {
+            return true;
+        }
+
+        if (id == R.id.action_info) {
+            Intent intent = new Intent(this, InfoActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
